1b5628ebd81b7def56a788253b39e976b4d30e97
Danilo da Silva Borges <danilosilva2017@hotmail.com>
2022-07-29 14:07:31 -0300

    Primeiro Projeto Angular

--- Introducao/primeiro-projeto/src/app/app.module.ts
+++ Introducao/primeiro-projeto/src/app/app.module.ts
@@ -4,20 +4,17 @@ import { FormsModule } from '@angular/forms';
 import { HttpModule } from '@angular/http';
 
 import { AppComponent } from './app.component';
-import { CursosComponent } from './cursos/cursos.component';
-import { CursosService } from './cursos/cursos.service';
 
 @NgModule({
   declarations: [
-    AppComponent,
-    CursosComponent        
+    AppComponent
   ],
   imports: [
     BrowserModule,
     FormsModule,
-    HttpModule,    
+    HttpModule
   ],
-  providers: [CursosService],
+  providers: [],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
