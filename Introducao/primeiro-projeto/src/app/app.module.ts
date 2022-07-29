fc43c446cd24dab5c21eab26fb4bdf2a19393236
Danilo da Silva Borges <danilosilva2017@hotmail.com>
2022-07-29 14:53:27 -0300

    Primeiro Projeto Angular

--- Introducao/primeiro-projeto/src/app/app.module.ts
+++ Introducao/primeiro-projeto/src/app/app.module.ts
@@ -4,10 +4,14 @@ import { FormsModule } from '@angular/forms';
 import { HttpModule } from '@angular/http';
 
 import { AppComponent } from './app.component';
+import { MeuPrimeiroComponente } from './meu-primeiro/meu-primeiro.component';
+import { MeuPrimeiro2Component } from './meu-primeiro2/meu-primeiro2.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    MeuPrimeiroComponente,
+    MeuPrimeiro2Component
   ],
   imports: [
     BrowserModule,
---
+++ Introducao/primeiro-projeto/src/app/meu-primeiro/meu-primeiro.component.ts
@@ -0,0 +1,9 @@
+import { Component } from '@angular/core';
+
+@Component({
+    selector: "app-meu-primeiro-componente",
+    template: `
+    <p>Meu primeiro componente com Angular 2!</p>
+    `
+})
+export class MeuPrimeiroComponente {}
