79a4444110e0ebfdb391d449adea0d7a75c9b564
Danilo da Silva Borges <danilosilva2017@hotmail.com>
2022-08-25 13:44:38 -0300

    Praticando com a criação básica de projetos Angular 2

--- Introducao/primeiro-projeto/src/app/meu-primeiro/meu-primeiro.component.ts
+++
@@ -1,9 +0,0 @@
-import { Component } from '@angular/core';
-
-@Component({
-    selector: "app-meu-primeiro-componente",
-    template: `
-    <p>Meu primeiro componente com Angular 2!</p>
-    `
-})
-export class MeuPrimeiroComponente {}
