0e27be9050fb7746f4e145174196bc2103802909
Danilo da Silva Borges <danilosilva2017@hotmail.com>
2022-09-20 10:20:25 -0300

    Iniciado Novo Projeto

---
+++ data-binding2/src/app/data-binding/data-binding.component.ts
@@ -0,0 +1,18 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-data-binding',
+  templateUrl: './data-binding.component.html',
+  styleUrls: ['./data-binding.component.sass']
+})
+export class DataBindingComponent implements OnInit {
+
+  public msg: string = "SisControle Online";
+  public urlIMagem: string = "https://picsum.photos/200/300";
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
